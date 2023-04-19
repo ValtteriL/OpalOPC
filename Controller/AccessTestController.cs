f863dacf70af96d63056df4cac77be4029ad3ef9
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-04-19 19:20:25 +0300

    Continue on TestAuth

--- Controller/AccessTestController.cs
+++ Controller/AccessTestController.cs
@@ -11,27 +11,30 @@ namespace Controller
         // populate opcTarget with auth test results
         public static OpcTarget TestAuth(OpcTarget opcTarget)
         {
-            IEnumerable<OpcTarget.Endpoint> kkk = opcTarget.GetEndpointsBySecurityMode(MessageSecurityMode.None);
-            foreach (OpcTarget.Endpoint f in kkk)
+            // TODO: CHECK ANONYMOUS AUTH
+            // try anonymous authentication
+            IEnumerable<OpcTarget.Endpoint> anonymousEndpoints = opcTarget.GetEndpointsByUserTokenType(UserTokenType.Anonymous);
+            foreach (OpcTarget.Endpoint endpoint in anonymousEndpoints)
             {
-                Console.WriteLine($"SecurityMode NONE {f.EndpointUrl}");
+                Console.WriteLine($"ANONYMOUS {endpoint.EndpointUrl}");
             }
-
-            IEnumerable<OpcTarget.Endpoint> rrr = opcTarget.GetEndpointsByUserTokenType(UserTokenType.Anonymous);
-            foreach (OpcTarget.Endpoint f in rrr)
-            {
-                Console.WriteLine($"ANONYMOUS {f.EndpointUrl}");
-            }
-
-            IEnumerable<OpcTarget.Endpoint> www = opcTarget.GetEndpointsBySecurityPolicyUri(SecurityPolicies.None);
-            foreach (OpcTarget.Endpoint f in www)
-            {
-                Console.WriteLine($"Securitypolicy NONE {f.EndpointUrl}");
-            }
-
 
             // TODO: CHECK COMMON CREDENTIALS - if username-pass
+            // brute username - pass
+            IEnumerable<OpcTarget.Endpoint> usernameEndpoints = opcTarget.GetEndpointsByUserTokenType(UserTokenType.UserName);
+            foreach (OpcTarget.Endpoint endpoint in usernameEndpoints)
+            {
+                Console.WriteLine($"USERNAME {endpoint.EndpointUrl}");
+            }
+
             // TODO: CHECK self signed cert - if certificate
+            // try authentication with self-signed certificate
+            IEnumerable<OpcTarget.Endpoint> certificateEndpoints = opcTarget.GetEndpointsByUserTokenType(UserTokenType.Certificate);
+            foreach (OpcTarget.Endpoint endpoint in certificateEndpoints)
+            {
+                Console.WriteLine($"CERTIFICATE {endpoint.EndpointUrl}");
+            }
+
             return null;
         }
 
