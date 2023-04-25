83f912cdcdb48973dc61ad3b8b4fd726ea0a53da
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-04-25 12:16:45 +0300

    Remove unnecessary artifacts from reference client

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
 using System.Globalization;
 using Controller;
-using Microsoft.Extensions.Logging;
 using Model;
 using Opc.Ua;
-using Opc.Ua.Configuration;
 
-namespace Quickstarts.ConsoleReferenceClient
+namespace OpcUaSecurityScanner
 {
-    /// <summary>
-    /// The program.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// Main entry point.
-        /// </summary>
         public static async Task Main(string[] args)
         {
             TextWriter output = Console.Out;
@@ -25,230 +17,16 @@ namespace Quickstarts.ConsoleReferenceClient
                 Utils.GetAssemblyTimestamp().ToString("G", CultureInfo.InvariantCulture),
                 Utils.GetAssemblySoftwareVersion());
 
-            // The application name and config file names
-            var applicationName = "ConsoleReferenceClient";
-            var configSectionName = "Quickstarts.ReferenceClient";
-            var usage = $"Usage: dotnet {applicationName}.dll [OPTIONS]";
+            ICollection<OpcTarget> targets = DiscoveryController.DiscoverTargets(new Uri("opc.tcp://echo.koti.kontu:53530"));
+            ICollection<OpcTarget> testedTargets = new List<OpcTarget>();
 
-            // command line options
-            bool showHelp = false;
-            bool autoAccept = false;
-            string? username = null;
-            string? userpassword = null;
-            bool logConsole = false;
-            bool appLog = false;
-            bool renewCertificate = false;
-            bool loadTypes = false;
-            bool browseall = false;
-            bool fetchall = false;
-            bool jsonvalues = false;
-            bool verbose = false;
-            string? password = null;
-            int timeout = Timeout.Infinite;
-            string? logFile = null;
-
-            Mono.Options.OptionSet options = new Mono.Options.OptionSet {
-                usage,
-                { "h|help", "show this message and exit", h => showHelp = h != null },
-                { "a|autoaccept", "auto accept certificates (for testing only)", a => autoAccept = a != null },
-                { "un|username=", "the name of the user identity for the connection", (string u) => username = u },
-                { "up|userpassword=", "the password of the user identity for the connection", (string u) => userpassword = u },
-                { "c|console", "log to console", c => logConsole = c != null },
-                { "l|log", "log app output", c => appLog = c != null },
-                { "p|password=", "optional password for private key", (string p) => password = p },
-                { "r|renew", "renew application certificate", r => renewCertificate = r != null },
-                { "t|timeout=", "timeout in seconds to exit application", (int t) => timeout = t * 1000 },
-                { "logfile=", "custom file name for log output", l => { if (l != null) { logFile = l; } } },
-                { "lt|loadtypes", "Load custom types", lt => { if (lt != null) loadTypes = true; } },
-                { "b|browseall", "Browse all references", b => { if (b != null) browseall = true; } },
-                { "f|fetchall", "Fetch all nodes", f => { if (f != null) fetchall = true; } },
-                { "j|json", "Output all Values as JSON", j => { if (j != null) jsonvalues = true; } },
-                { "v|verbose", "Verbose output", v => { if (v != null) verbose = true; } },
-            };
-
-            try
+            foreach (OpcTarget target in targets)
             {
-                // parse command line and set options
-                var extraArg = ConsoleUtils.ProcessCommandLine(output, args, options, ref showHelp, false);
-
-                // connect Url?
-                Uri serverUrl = new Uri("opc.tcp://localhost:62541/Quickstarts/ReferenceServer");
-                if (!string.IsNullOrEmpty(extraArg))
-                {
-                    serverUrl = new Uri(extraArg);
-                }
-
-                // log console output to logger
-                if (logConsole && appLog)
-                {
-                    output = new LogWriter();
-                }
-
-                // Define the UA Client application
-                ApplicationInstance.MessageDlg = new ApplicationMessageDlg(output);
-                CertificatePasswordProvider PasswordProvider = new CertificatePasswordProvider(password);
-                ApplicationInstance application = new ApplicationInstance
-                {
-                    ApplicationName = applicationName,
-                    ApplicationType = ApplicationType.Client,
-                    ConfigSectionName = configSectionName,
-                    CertificatePasswordProvider = PasswordProvider
-                };
-
-                // load the application configuration.
-                var config = await application.LoadApplicationConfiguration(silent: false);
-
-                // override logfile
-                if (logFile != null)
-                {
-                    var logFilePath = config.TraceConfiguration.OutputFilePath;
-                    var filename = Path.GetFileNameWithoutExtension(logFilePath);
-                    config.TraceConfiguration.OutputFilePath = logFilePath.Replace(filename, logFile);
-                    config.TraceConfiguration.DeleteOnLoad = true;
-                    config.TraceConfiguration.ApplySettings();
-                }
-
-                // setup the logging
-                ConsoleUtils.ConfigureLogging(config, applicationName, logConsole, LogLevel.Information);
-
-                // delete old certificate
-                if (renewCertificate)
-                {
-                    await application.DeleteApplicationInstanceCertificate().ConfigureAwait(false);
-                }
-
-                // check the application certificate.
-                bool haveAppCertificate = await application.CheckApplicationInstanceCertificate(false, minimumKeySize: 0).ConfigureAwait(false);
-                if (!haveAppCertificate)
-                {
-                    throw new ErrorExitException("Application instance certificate invalid!", ExitCode.ErrorCertificate);
-                }
-
-                // wait for timeout or Ctrl-C
-                var quitEvent = ConsoleUtils.CtrlCHandler();
-
-                // connect to a server until application stops
-                bool quit = false;
-                DateTime start = DateTime.UtcNow;
-                int waitTime = int.MaxValue;
-                do
-                {
-                    if (timeout > 0)
-                    {
-                        waitTime = timeout - (int)DateTime.UtcNow.Subtract(start).TotalMilliseconds;
-                        if (waitTime <= 0)
-                        {
-                            break;
-                        }
-                    }
-
-                    ICollection<OpcTarget> targets = DiscoveryController.DiscoverTargets(new Uri("opc.tcp://echo.koti.kontu:53530"));
-                    ICollection<OpcTarget> testedTargets = new List<OpcTarget>();
-
-                    foreach(OpcTarget target in targets)
-                    {
-                        testedTargets.Add(SecurityTestController.TestOpcTargetSecurity(target));
-                        Console.WriteLine(target);
-                    }
-
-                    ReportController.GenerateReport(testedTargets);
-
-                    return;
-
-                    // create the UA Client object and connect to configured server.
-                    using (UAClient uaClient = new UAClient(
-                        application.ApplicationConfiguration, output, ClientBase.ValidateResponse)
-                    {
-                        AutoAccept = autoAccept
-                    })
-                    {
-
-                        // set user identity
-                        if (!String.IsNullOrEmpty(username))
-                        {
-                            // identity can be configured to be anonymous, username, certificate, token
-                            uaClient.UserIdentity = new UserIdentity(username, userpassword ?? string.Empty);
-                        }
-
-                        bool connected = await uaClient.ConnectAsync(serverUrl.ToString(), false);
-                        if (connected)
-                        {
-                            output.WriteLine("Connected! Ctrl-C to quit.");
-
-                            // enable subscription transfer
-                            uaClient.Session.TransferSubscriptionsOnReconnect = true;
-
-                            var samples = new ClientSamples(output, ClientBase.ValidateResponse, quitEvent, verbose);
-                            if (loadTypes)
-                            {
-                                await samples.LoadTypeSystem(uaClient.Session).ConfigureAwait(false);
-                            }
-
-                            if (browseall || fetchall || jsonvalues)
-                            {
-                                NodeIdCollection? variableIds = null;
-                                ReferenceDescriptionCollection? referenceDescriptions = null;
-                                if (browseall)
-                                {
-                                    referenceDescriptions =
-                                        samples.BrowseFullAddressSpace(uaClient, Objects.RootFolder);
-                                    variableIds = new NodeIdCollection(referenceDescriptions
-                                        .Where(r => r.NodeClass == NodeClass.Variable && r.TypeDefinition.NamespaceIndex != 0)
-                                        .Select(r => ExpandedNodeId.ToNodeId(r.NodeId, uaClient.Session.NamespaceUris)));
-                                }
-
-                                IList<INode>? allNodes = null;
-                                if (fetchall)
-                                {
-                                    allNodes = samples.FetchAllNodesNodeCache(
-                                        uaClient, Objects.RootFolder, true, true, false);
-                                    variableIds = new NodeIdCollection(allNodes
-                                        .Where(r => r.NodeClass == NodeClass.Variable && ((VariableNode)r).DataType.NamespaceIndex != 0)
-                                        .Select(r => ExpandedNodeId.ToNodeId(r.NodeId, uaClient.Session.NamespaceUris)));
-                                }
-
-                                if (jsonvalues && variableIds != null)
-                                {
-                                    await samples.ReadAllValuesAsync(uaClient, variableIds);
-                                }
-
-                                quit = true;
-                            }
-                            else
-                            {
-                                // Run tests for available methods on reference server.
-                                samples.ReadNodes(uaClient.Session);
-                                samples.WriteNodes(uaClient.Session);
-                                samples.Browse(uaClient.Session);
-                                samples.CallMethod(uaClient.Session);
-                                samples.SubscribeToDataChanges(uaClient.Session, 120_000);
-
-                                output.WriteLine("Waiting...");
-
-                                // Wait for some DataChange notifications from MonitoredItems
-                                quit = quitEvent.WaitOne(timeout > 0 ? waitTime : 30_000);
-                            }
-
-                            output.WriteLine("Client disconnected.");
-
-                            uaClient.Disconnect();
-                        }
-                        else
-                        {
-                            output.WriteLine("Could not connect to server! Retry in 10 seconds or Ctrl-C to quit.");
-                            quit = quitEvent.WaitOne(Math.Min(10_000, waitTime));
-                        }
-                    }
-
-                } while (!quit);
-
-                output.WriteLine("Client stopped.");
+                testedTargets.Add(SecurityTestController.TestOpcTargetSecurity(target));
+                Console.WriteLine(target);
             }
-            catch (Exception ex)
-            {
-                output.WriteLine(ex.Message);
-                throw ex;
-            }
+
+            ReportController.GenerateReport(testedTargets);
         }
     }
 }
