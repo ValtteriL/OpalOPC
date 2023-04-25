bd7fb76dc02d6e950fb3688381154346f568060b
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-04-25 13:37:34 +0300

    Remove redundant check

--- Util/ConnectionUtil.cs
+++ Util/ConnectionUtil.cs
@@ -54,10 +54,6 @@ namespace Util
                 null
             ).ConfigureAwait(false);
 
-            if (!session.Connected)
-            {
-                return null;
-            }
             return session;
         }
     }
