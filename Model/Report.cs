07f04dcfc613c3c84c7479947e1ff7aec3914ceb
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-04-28 14:27:36 +0300

    Fix: make Timestamp visible in reports

--- Model/Report.cs
+++ Model/Report.cs
@@ -1,13 +1,15 @@
+using System.Xml.Serialization;
+
 namespace Model
 {
     public class Report
     {
         public List<Target> Targets { get; private set; } = new List<Target>();
-        DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }
 
         // parameterless constructor for XML serializer
         internal Report()
-        {}
+        { }
 
         public Report(ICollection<Target> opcTargets)
         {
