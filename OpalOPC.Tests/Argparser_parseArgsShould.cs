1404ac74484db203ca74b2c4e9d736659f02d623
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-05-22 10:16:56 +0300

    Add Argparser tests

--- OpalOPC.Tests/Argparser_parseArgsShould.cs
+++ OpalOPC.Tests/Argparser_parseArgsShould.cs
@@ -6,14 +6,59 @@ using Xunit;
 public class Argparser_parseArgsShould
 {
     [Fact]
-    public void ParseArgs_InputOutputFileNameMatchesOptionsReportName()
+    public void ParseArgs_NoTargets_ResultsInNoTargets()
     {
-        const string filename = "a";
-        string[] args = {"-o", filename};
+        string[] args = { "-v" };
         Argparser argparser = new Argparser(args);
-        
+
+        Options options = argparser.parseArgs();
+
+        Assert.True(options.targets.Count == 0);
+    }
+
+    [Theory]
+    [InlineData("-o", "a")]
+    [InlineData("--output", "b")]
+    public void ParseArgs_InputOutputFileNameMatchesOptionsReportName(string flag, string filename)
+    {
+        string[] args = { flag, filename };
+        Argparser argparser = new Argparser(args);
+
         Options options = argparser.parseArgs();
 
         Assert.True(options.xmlOutputReportName == filename);
+    }
+
+    [Fact]
+    public void ParseArgs_IncreaseVerbosity1()
+    {
+        string[] args = { "-v" };
+        Argparser argparser = new Argparser(args);
+
+        Options options = argparser.parseArgs();
+
+        Assert.True(options.logLevel == Microsoft.Extensions.Logging.LogLevel.Debug);
+    }
+
+    [Fact]
+    public void ParseArgs_IncreaseVerbosity2()
+    {
+        string[] args = { "-v", "-v" };
+        Argparser argparser = new Argparser(args);
+
+        Options options = argparser.parseArgs();
+
+        Assert.True(options.logLevel == Microsoft.Extensions.Logging.LogLevel.Trace);
+    }
+
+    [Fact]
+    public void ParseArgs_SilenceLogging()
+    {
+        string[] args = { "-s" };
+        Argparser argparser = new Argparser(args);
+
+        Options options = argparser.parseArgs();
+
+        Assert.True(options.logLevel == Microsoft.Extensions.Logging.LogLevel.None);
     }
 }
