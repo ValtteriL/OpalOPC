4c3ecb1e361b1f78232e849301aebd23af62ff32
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-05-22 10:36:06 +0300

    Add Reporter tests

---
+++ OpalOPC.Tests/Reporter_printXMLReportShould.cs
@@ -0,0 +1,39 @@
+namespace Tests;
+using View;
+using Model;
+using Xunit;
+
+public class Reporter_printXMLReportShould
+{
+    [Fact]
+    public void printXMLReport_NullReportCausesNullReferenceException()
+    {
+        StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+        sw.AutoFlush = true;
+        Reporter reporter = new Reporter(sw.BaseStream);
+        Report? report = null;
+
+        try
+        {
+            reporter.printXMLReport(report!);
+        }
+        catch (System.NullReferenceException)
+        {
+            Assert.True(true);
+            return;
+        }
+
+        Assert.True(false);
+    }
+
+    [Fact]
+    public void printXMLReport_NonNullReportSucceeds()
+    {
+        StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+        sw.AutoFlush = true;
+        Reporter reporter = new Reporter(sw.BaseStream);
+        Report report = new Report(new List<Target>(), DateTime.Now, DateTime.Now);
+
+        reporter.printXMLReport(report);
+    }
+}
