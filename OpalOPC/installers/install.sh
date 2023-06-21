51b0e564e2b698d5a2f8ee86bb55e451aa150d1c
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-06-21 21:26:49 +0300

    Add installer status, banner

--- OpalOPC/installers/install.sh
+++ OpalOPC/installers/install.sh
@@ -38,23 +38,45 @@ is_mac () { [[ $OSTYPE == 'darwin'* ]]; }
 
 # Checks to see if the given command (passed as a string argument) exists on the system.
 # The function returns 0 (success) if the command exists, and 1 if it doesn't.
-is_command() {
+is_command () {
     local check_command="$1"
     command -v "${check_command}" >/dev/null 2>&1
 }
 
+print_banner () {
+    echo '
+ .d88888b.                    888  .d88888b.  8888888b.   .d8888b.  
+d88P" "Y88b                   888 d88P" "Y88b 888   Y88b d88P  Y88b 
+888     888                   888 888     888 888    888 888    888 
+888     888 88888b.   8888b.  888 888     888 888   d88P 888        
+888     888 888 "88b     "88b 888 888     888 8888888P"  888        
+888     888 888  888 .d888888 888 888     888 888        888    888 
+Y88b. .d88P 888 d88P 888  888 888 Y88b. .d88P 888        Y88b  d88P 
+ "Y88888P"  88888P"  "Y888888 888  "Y88888P"  888         "Y8888P"  
+            888                                                     
+            888                                                     
+            888                                                     
+    '
+}
+
 ### Program
 
+print_banner
+echo ""
+
 # Check that script is run as root
+echo "[1/6] Checking if installer run as root"
 if ! is_user_root; then
     echo "Installer must be run as root"
     exit 1
 fi
 
 # Create directory
+echo "[2/6] Creating installation directory (${INSTALL_DIR})"
 mkdir -p "${INSTALL_DIR}"
 
 # Download latest OpalOPC
+echo "[3/6] Downloading Opal OPC"
 if is_mac; then
     OPALOPC_URI="${MAC_LATEST_OPALOPC_URI}"
 else
@@ -63,6 +85,7 @@ fi
 wget --quiet --output-document "${OPALOPC_EXECUTABLE_PATH}" "${OPALOPC_URI}"
 
 # Check signature
+echo "[4/6] Checking signature"
 if is_command gpg ; then
     wget -qO - "${GPG_KEY_URI}" | gpg --quiet --import -
 
@@ -79,9 +102,12 @@ else
 fi
 
 # Make executable
+echo "[5/6] Making Opal OPC executable"
 chmod +x "${OPALOPC_EXECUTABLE_PATH}"
 
 # Add to path
+echo "[6/6] Adding \"opalopc\" to path"
 ln --symbolic --force "${OPALOPC_EXECUTABLE_PATH}" "${OPALOPC_USR_LOCAL_BIN_SYMLINK_PATH}"
 
+echo ""
 echo "Installation complete! Type opalopc to get started"
