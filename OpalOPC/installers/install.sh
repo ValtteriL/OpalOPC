dd2cb53a435af755aba0b45e08874b1313e4bf76
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-06-21 19:10:26 +0300

    Check that run as root

--- OpalOPC/installers/install.sh
+++ OpalOPC/installers/install.sh
@@ -5,7 +5,8 @@
 # (as many distros as possible) and MacOS. 
 #
 # After installation OpalOPC is found in /opt/opalopc/
-# and it is added to all user's paths through /etc/profile.d/opalopc-path.sh
+# and it is added to all user's paths (Borne Shell derivatives)
+# through /etc/profile.d/opalopc-path.sh
 # 
 # For uninstallation, just:
 # - delete /opt/opalopc/
@@ -14,10 +15,29 @@
 # Run as root.
 #
 
-echo "Hello world!"
+### Variables
+
+INSTALL_DIR="/opt/opalopc"
+PROFILE_FILE_PATH="/etc/profile.d/opalopc-path.sh"
+
+### Functions
+
+# bool function to test if the user is root or not
+is_user_root () { [ "${EUID:-$(id -u)}" -eq 0 ]; }
+
+### Program
+
+# Check that script is run as root
+if ! is_user_root; then
+    echo "Installer must be run as root"
+    exit 1
+fi
 
 # Create directory
 # Download latest OpalOPC
 # (Check signature)
 # Make executable
 # Add to path
+
+
+echo "Hello world!"
