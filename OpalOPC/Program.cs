55e7f26ae11e08b70526453b9b1fddda0adcda1b
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-07-14 14:08:14 +0300

    Move scanning logic to ScanController

--- OpalOPC/Program.cs
+++ OpalOPC/Program.cs
@@ -1,7 +1,6 @@
 using Controller;
 using Microsoft.Extensions.Logging;
 using Model;
-using Plugin;
 using View;
 
 
@@ -34,49 +33,8 @@ class OpalOPC
         VersionCheckController versionCheckController = new VersionCheckController(logger);
         versionCheckController.CheckVersion();
 
-        DateTime start = DateTime.Now;
-        logger.LogInformation($"Starting OpalOPC {Util.VersionUtil.AppAssemblyVersion} ( https://opalopc.com )");
-
-        if (options.targets.Count == 0)
-        {
-            logger.LogWarning("No targets were specified, so 0 applications will be scanned.");
-        }
-
-        IReporter reporter = new Reporter(options.xmlOutputStream!);
-
-        DiscoveryController discoveryController = new DiscoveryController(logger);
-        ICollection<Target> targets = discoveryController.DiscoverTargets(options.targets);
-
-        // Initialize security testing plugins
-        ICollection<IPlugin> securityTestPlugins = new List<IPlugin> {
-            new SecurityModeInvalidPlugin(logger),
-            new SecurityModeNonePlugin(logger),
-
-            new SecurityPolicyBasic128Rsa15Plugin(logger),
-            new SecurityPolicyBasic256Plugin(logger),
-            new SecurityPolicyNonePlugin(logger),
-
-            new AnonymousAuthenticationPlugin(logger),
-            new SelfSignedCertificatePlugin(logger),
-
-            new CommonCredentialsPlugin(logger),
-            new RBACNotSupportedPlugin(logger),
-            new AuditingDisabledPlugin(logger),
-        };
-
-        SecurityTestController securityTestController = new SecurityTestController(logger, securityTestPlugins);
-        ICollection<Target> testedTargets = securityTestController.TestTargetSecurity(targets);
-
-        ReportController reportController = new ReportController(logger, reporter);
-
-        DateTime end = DateTime.Now;
-        reportController.GenerateReport(testedTargets, start, end);
-
-        TimeSpan ts = (end - start);
-        string runStatus = $"OpalOPC done: {options.targets.Count} Discovery URLs ({reportController.report!.Targets.Count} applications found) scanned in {Math.Round(ts.TotalSeconds, 2)} seconds";
-        logger.LogInformation(runStatus);
-
-        reportController.WriteReport(runStatus);
+        ScanController scanController = new ScanController(logger, options.targets, options.xmlOutputStream!);
+        scanController.Scan();
 
         if (options.xmlOutputReportName != null)
         {
