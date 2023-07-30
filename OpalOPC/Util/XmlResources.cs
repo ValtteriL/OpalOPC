3e2873f490aa0f694af6022ed9d8059a27a0e91c
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-07-30 22:52:24 +0300

    Refer to dtd locally without webserver

--- OpalOPC/Util/XmlResources.cs
+++ OpalOPC/Util/XmlResources.cs
@@ -4,7 +4,7 @@ namespace Util
     {
 #if DEBUG
         public static string StylesheetLocation = "http://localhost:8000/OpalOPC/report-resources/stylesheet.xsl";
-        public static string DtdLocation = "http://localhost:8000/OpalOPC/report-resources/report.dtd";
+        public static string DtdLocation = "OpalOPC/report-resources/report.dtd";
 #else
         public static string StylesheetLocation = $"https://opalopc.com/report-resources/{Util.VersionUtil.AppAssemblyVersion!.ToString()}/stylesheet.xsl";
         public static string DtdLocation = $"https://opalopc.com/report-resources/{Util.VersionUtil.AppAssemblyVersion!.ToString()}/report.dtd";
