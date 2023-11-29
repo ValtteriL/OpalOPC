24498f26888007f0ff7f752ab7d02f7c70eb2971
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-11-29 20:25:58 +0200

    Simplify object creation

--- OpalOPC.Tests/ProvidedCredentialsPlugin_Test.cs
+++ OpalOPC.Tests/ProvidedCredentialsPlugin_Test.cs
@@ -27,13 +27,13 @@ public class ProvidedCredentialsPluginTest
             },
         applicationCertificates = new List<CertificateIdentifier>
         {
-                new CertificateIdentifier(CertificateBuilder.Create("CN=Test").CreateForRSA()),
-                new CertificateIdentifier(CertificateBuilder.Create("CN=Test").CreateForRSA())
+                new(CertificateBuilder.Create("CN=Test").CreateForRSA()),
+                new(CertificateBuilder.Create("CN=Test").CreateForRSA())
         },
         userCertificates = new List<CertificateIdentifier>
         {
-                new CertificateIdentifier(CertificateBuilder.Create("CN=Test").CreateForRSA()),
-                new CertificateIdentifier(CertificateBuilder.Create("CN=Test").CreateForRSA())
+                new(CertificateBuilder.Create("CN=Test").CreateForRSA()),
+                new(CertificateBuilder.Create("CN=Test").CreateForRSA())
         }
     };
 
