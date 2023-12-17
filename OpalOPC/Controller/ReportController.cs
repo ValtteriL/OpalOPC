2258957272d39c81acf4ef9702a5db1bf09b5a60
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2023-12-17 15:27:38 +0200

    Add detailed telemetry
    New event: Scan finished
    And added scan properties to Scan started

--- OpalOPC/Controller/ReportController.cs
+++ OpalOPC/Controller/ReportController.cs
@@ -9,17 +9,12 @@ namespace Controller
 
         readonly ILogger _logger;
         readonly IReporter _reporter;
-        public Report? report { get; private set; }
+        public Report report { get; private set; }
 
-        public ReportController(ILogger logger, IReporter reporter)
+        public ReportController(ILogger logger, IReporter reporter, ICollection<Target> targets, DateTime Start, DateTime End, string commandLine, string runStatus)
         {
             _reporter = reporter;
             _logger = logger;
-        }
-
-        // Reporter and targets, generate report
-        public void GenerateReport(ICollection<Target> targets, DateTime Start, DateTime End, string commandLine, string runStatus)
-        {
             _logger.LogDebug("{Message}", "Generating report");
             report = new Report(targets, Start, End, commandLine, runStatus);
         }
