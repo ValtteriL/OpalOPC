5dbc92c3f2678d7615ceaf9caaf3c38a463f3a00
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-01-26 22:00:52 +0200

    Start GUI e2e tests
    Following these:
    - https://github.com/microsoft/WinAppDriver
    - https://github.com/microsoft/WinAppDriver/blob/master/Docs/AuthoringTestScripts.md
    - https://techcommunity.microsoft.com/t5/modern-work-app-consult-blog/ui-testing-for-windows-apps-with-winappdriver-and-appium/ba-p/825352

---
+++ OpalOPC.Tests/GUI/Gui_Test.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Tests.GUI
+{
+    public class GuiTest : TestsBase, IDisposable
+    {
+        public GuiTest() : base() { }
+
+        public void Dispose()
+        {
+            Cleanup();
+            StopWinappDriver();
+        }
+
+        [Fact]
+        public void EndToEnd()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.True(true);
+        }
+    }
+}
