2655529493a09aed626a3912da560396ee063143
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-01-31 09:34:45 +0200

    Improve texts on ReportMain
    Also fix some CSS so that the list items are not messed up

--- website/src/components/ReportMain/ReportMain.js
+++ website/src/components/ReportMain/ReportMain.js
@@ -4,16 +4,16 @@ const ReportMain = () => {
   return (
     <div class="main-container">
         <div class="main-txt">
-            <h1><span>Reveal security issues</span> in OPC UA applications</h1>
-            <p>Securing systems requires exposing their weak spots</p>
+            <h1><span>Reveal Security Issues</span> in your Most Critical Systems</h1>
+            <p>OPC UA security scanner</p>
             <div class="main-btns">
                 <a href="/docs/get-started/quick-start" class="primary-btn common-btns">Start Scanning</a>
                 <a href="/docs" class="secondary-btn common-btns">Read Docs</a>
             </div>
             <ul>
-                <li><span></span>Amet minim mollit non honyiq</li>
-                <li><span></span>Amet lorem minim hjusza non deserunt</li>
-                <li><span></span>Amet mollit non deserunt</li>
+                <li><span></span>From download to testing report in <b>&lt;5min</b></li>
+                <li><span></span>State of the art security, even with <b>zero OPC UA knowledge</b></li>
+                <li><span></span>Say goodbye to unactionable results - <b>clear</b> prioritization &amp; fixing plan</li>
             </ul>
             <div class='dwnld-links'>
                 <a href="https://apps.microsoft.com/detail/9N89VWR0GK7H?launch=true&mode=mini&hl=en-us&gl=US" target="_blank" class="microsoft-link"><img src="/img/microsoft.jpg" alt="Microsoft" /></a>
