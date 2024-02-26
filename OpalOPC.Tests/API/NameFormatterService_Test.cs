0e67267d2926a7ae39af640c55a6487c86e23313
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-02-26 08:58:38 +0200

    Improve tests

--- OpalOPC.Tests/API/NameFormatterService_Test.cs
+++ OpalOPC.Tests/API/NameFormatterService_Test.cs
@@ -3,20 +3,33 @@ using Xunit;
 
 namespace OpalOPC.Tests.API
 {
-    public class NameFormatterServiceTest(NameFormatterService nameFormatterService)
+    public class NameFormatterServiceTest()
     {
 
+        public static IEnumerable<object[]> TestData =>
+            [
+                ["Vendor Name", new object[] { "Vendor", "Name" }],
+                ["Vendor!@#$%^&*()_+Name", new object[] { "Vendor", "Name" }],
+                ["hello123", new string[] { "hello", "123" }],
+                ["vendorName", new string[] { "vendor", "Name" }],
+                ["VendorName", new string[] { "Vendor", "Name" }],
+                ["Prosys OPC Ltd.", new string[] { "Prosys", "OPC", "Ltd" }],
+                ["SimulationServer@echo", new string[] { "Simulation", "Server", "echo" }],
+                ["this is too long string 123 kek", new string[] { "this", "is", "too", "long", "string" }],
+            ];
+
+        [Fact]
+        public void TestNameFormatterService()
+        {
+            INameFormatterService nameFormatterService = new NameFormatterService();
+            Assert.NotNull(nameFormatterService);
+        }
+
         [Theory]
-        [InlineData("Vendor Name", new string[] { "Vendor", "Name" })]
-        [InlineData("Vendor!@#$%^&*()_+Name", new string[] { "Vendor", "Name" })]
-        [InlineData("hello123", new string[] { "hello", "123" })]
-        [InlineData("vendorName", new string[] { "vendor", "Name" })]
-        [InlineData("VendorName", new string[] { "Vendor", "Name" })]
-        [InlineData("Prosys OPC Ltd.", new string[] { "Prosys", "OPC", "Ltd" })]
-        [InlineData("SimulationServer@echo", new string[] { "Simulation", "Server", "echo" })]
-        [InlineData("this is too long string 123 kek", new string[] { "this", "is", "too", "long", "string" })]
+        [MemberData(nameof(TestData))]
         public void CorrectResult(string input, string[] expectedOutput)
         {
+            NameFormatterService nameFormatterService = new NameFormatterService();
             string[] result = nameFormatterService.FormatName(input);
             Assert.Equal(expectedOutput, result);
         }
