fc952a3528ffd9b1d52c7f09bf3cb3c0dfaef72b
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-03-20 15:35:37 +0200

    Delete unused component

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -12,32 +12,6 @@ import Head from '@docusaurus/Head';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  // const {siteConfig} = useDocusaurusContext();
-  
-  return (
-    <div>
-      <HomepageHeader />
-    </div>
-    // <header className={clsx('hero hero--primary', styles.heroBanner)}>
-    //   <div className="container">
-    //     
-    //     <Heading as="h1" className="hero__title">
-    //       {siteConfig.title}
-    //     </Heading>
-    //     <p className="hero__subtitle">{siteConfig.tagline}</p>
-    //     <div className={styles.buttons}>
-    //       <Link
-    //         className="button button--secondary button--lg"
-    //         to="/docs">
-    //         Docusaurus Tutorial - 5min ⏱️
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </header>
-  );
-}
-
 // Create component that adds og:image for frontpage
 // https://docusaurus.io/docs/next/docusaurus-core#head
 const MySEO = () => (
