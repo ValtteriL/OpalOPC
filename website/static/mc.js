41b15476c27f6ab91d5636542ca1712dbe2bb7b3
Valtteri Lehtinen <valtteri@shufflingbytes.com>
2024-03-20 15:22:34 +0200

    Remove failing Mailchimp script

--- website/static/mc.js
+++
@@ -1,1 +0,0 @@
-!function (c, h, i, m, p) { m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p) }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/b8481fa110fd7bae3191c7b5f/c89d851cebe90d14cf64730b7.js");
